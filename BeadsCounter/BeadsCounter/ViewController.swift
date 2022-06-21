1d0a1627c5ab30def13736d3e317aaa510b82d05
fedir72 <fedir72@ua.fm>
2022-06-21 19:34:17 +0300

    empty project

---
+++ BeadsCounter/BeadsCounter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeadsCounter
+//
+//  Created by Fedii Ihor on 21.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
